32fa46861f76a6173e04ad1e7326f9333e18645c
dnlgolding <38785200-dnlgolding@users.noreply.replit.com>
2025-01-29 20:18:23 +0000

    Agent query: Are both images now displaying with the same height?
    Increase size of logo images on home page.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/969c0737-d469-4bc5-8b1d-2f3b7039a9d5/b4b2b90e-909e-4cd7-8480-736154de402d.jpg

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <img 
         src="/assets/cornerlogo.png" 
         alt="Where is the start?" 
-        className="absolute top-4 left-4 h-16 w-auto"
+        className="absolute top-4 left-4 h-48 w-auto"
       />
 
       <div className="max-w-2xl mx-auto p-4">
@@ -33,7 +33,7 @@ export default function Home() {
           <img 
             src="/assets/Happening.png" 
             alt="Entrepreneurship starts" 
-            className="w-full max-w-lg h-auto"
+            className="w-full max-w-lg h-48"
           />
         </div>
 
